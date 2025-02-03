785510a79cba518861723b22e0458cbca6abba47
Poliany <poliany.g@hotmail.com>
2025-02-03 17:45:43 -0300

    Lista, agregar nomes de amigos e limpar campo

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let listaDeAmigos = [];
+
+function adicionarAmigo() {
+    if (document.getElementById('amigo').value === '') {
+        alert('Por favor, insira um nome.');
+    }  else {
+        let nome = document.getElementById('amigo').value;
+        listaDeAmigos.push(nome);
+        console.log(listaDeAmigos); 
+        limparCampo();
+    } 
+}
+
+        function limparCampo() {
+            let zerar = document.querySelector('input');
+            zerar.value = '';
+        }
+            
